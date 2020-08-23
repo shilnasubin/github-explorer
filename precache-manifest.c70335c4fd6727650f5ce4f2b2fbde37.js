492b9a291fa8e57e360105769ffb8ed3ea87defb
shilna <subindev@outlook.com>
2020-08-23 14:04:46 +0400

    Updates

---
+++ precache-manifest.c70335c4fd6727650f5ce4f2b2fbde37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443ba81985a4e010abc9af993f547e43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61800c5d55ab1ed19550",
+    "url": "/static/css/main.f956a075.chunk.css"
+  },
+  {
+    "revision": "53c2fb3889bce0fefed4",
+    "url": "/static/js/2.12b46955.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.12b46955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61800c5d55ab1ed19550",
+    "url": "/static/js/main.96f4f53e.chunk.js"
+  },
+  {
+    "revision": "2e0e95700a5ba086426d",
+    "url": "/static/js/runtime-main.9beb02a6.js"
+  }
+]);
